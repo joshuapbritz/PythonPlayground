b087d9d41df96dd00bc75e9c3c292a82ae790a5f
Josh <josh@Creachaanganayi.lan>
2018-07-31 15:47:52 +0200

    oop

---
+++ oop.py
@@ -0,0 +1,20 @@
+## OOP Programming
+# class User():
+#     pass
+
+# instance = User()
+
+# print(type(instance))
+
+# class User():
+#     def __init__(self, username, email, password, name, surname):
+#         self.username = username
+#         self.email = email
+#         self.password = password,
+#         self.name = name,
+#         self.surname = surname
+#         pass
+
+# instance = User('joshuabritz', 'joshuabritz@gmail.com', 'password1234', 'Joshua', 'Britz')
+
+# print(instance)
