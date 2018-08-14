ae6e9a0e304af389e87a91ffbc1405fded510a0d
Josh <josh@Creachaanganayi.lan>
2018-08-14 16:19:14 +0200

    Pylint

--- geometry.py
+++ geometry.py
@@ -1,7 +1,13 @@
+'''
+Geometry module for working with shapes
+'''
 from math import sqrt, pi
 
 
 class Line():
+    '''
+    A Class for working with lines
+    '''
     def __init__(self, coords_1, coords_2):
         self.coords_1 = coords_1
         self.coords_2 = coords_2
@@ -11,37 +17,53 @@ class Line():
         }
 
     def distance(self):
+        '''
+        Get the distance between two points
+        '''
         return round(
             sqrt((self.diffs.get('x')**2) + (self.diffs.get('y')**2)), 2)
 
     def slope(self):
+        '''
+        Get the slope of a line
+        '''
         return round(self.diffs.get('y') / self.diffs.get('x'), 2)
 
 
-c1 = (1, 8)
-c2 = (8, 10)
+C1 = (1, 8)
+C2 = (8, 10)
 
-line = Line(c1, c2)
+LINE = Line(C1, C2)
 
-print(line.slope())
-print(line.distance())
+print(LINE.slope())
+print(LINE.distance())
 
 
 ## Cylinder
 class Cylinder():
+    '''
+    A clas for working with cylinders
+    '''
+
     def __init__(self, height=1, radius=1):
         self.height = height
         self.radius = radius
 
     def volume(self):
+        '''
+        Get the voume of the cylinder
+        '''
         return round(pi * (self.radius**2) * self.height, 2)
 
     def surface_area(self):
+        '''
+        Get the surface area of the cylinder
+        '''
         return round(((2 * pi) * self.radius * self.height) +
                      (2 * pi * (self.radius**2)), 2)
 
 
-cylinder = Cylinder(2, 3)
+CYLINDER = Cylinder(2, 3)
 
-print(cylinder.volume())
-print(cylinder.surface_area())
+print(CYLINDER.volume())
+print(CYLINDER.surface_area())
